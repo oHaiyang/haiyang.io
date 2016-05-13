b94e1bcf3754dea825c7224fefaa2c4b545a076f
oHaiyang <phycgp@live.com>
2016-05-13 11:18:55 +0800

    some improvements

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var replace = require('gulp-replace');
+
+gulp.task('deploy', function () {
+:);
+
+gulp.task('default', ['serve']);
