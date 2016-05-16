337c48036712d934eb145a1d89f0f4ea61fc8c1a
oHaiyang <phycgp@live.com>
2016-05-16 19:28:41 +0800

    UI improvement and intergrated fontmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,38 @@
 'use strict';
 
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync');
-var reload = browserSync.reload;
-var replace = require('gulp-replace');
+var fontmin = require('gulp-fontmin');
+var htmlToText = require('html-to-text');
+var Fontmin = require('fontmin');
 
-gulp.task('deploy', function () {
-:);
+gulp.task('font', function() {
+
+	var buffers = [];
+
+	gulp.src("haiyang.io/*.html")
+		.on('data', function(file) {
+			buffers.push(file.contents);
+		})
+		.on('end', function() {
+			var text = htmlToText.fromString(Buffer.concat(buffers).toString('utf-8'));
+      console.log("the text: " + text);
+			var fontmin = new Fontmin()
+				.src('HYQuanTangShiJ.ttf')
+				.use(Fontmin.glyph({
+					text: text,
+					hinting: false // keep ttf hint info (fpgm, prep, cvt). default = true
+				}))
+				.use(Fontmin.ttf2woff())
+				.dest('haiyang.io/');
+			fontmin.run(function(err, files) {
+				if (err) {
+					throw err;
+				}
+
+				console.log(files[0]);
+				// => { contents: <Buffer 00 01 00 ...> }
+			});
+		});
+});
 
 gulp.task('default', ['serve']);
